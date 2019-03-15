85b11413ae01900b27f08a662ce12d3c6d826960
Karol Leszczyński <karol.leszczynski@gmail.com>
2019-03-15 01:24:57 +0100

    some config files added

---
+++ src/main/java/pl/karoll/issInfo/IssInfoApplication.java
@@ -0,0 +1,13 @@
+package pl.karoll.issInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssInfoApplication.class, args);
+	}
+
+}
